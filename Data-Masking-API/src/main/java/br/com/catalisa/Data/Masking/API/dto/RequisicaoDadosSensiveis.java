cb14e0c6970e21908fc5bf9f5145c349b31b43f6
[AleDomici] <alessandra.domiciano@zup.com.br>
2025-05-14 16:33:13 -0300

    feat(dto): adicionar construtores à DTO RequisicaoDadosSensiveis
    Adicionar construtor vazio para frameworks que exigem instância padrão
    Adicionar construtor com argumentos cpf e cartao para facilitar criação do objeto

--- Data-Masking-API/src/main/java/br/com/catalisa/Data/Masking/API/dto/RequisicaoDadosSensiveis.java
+++ Data-Masking-API/src/main/java/br/com/catalisa/Data/Masking/API/dto/RequisicaoDadosSensiveis.java
@@ -3,25 +3,23 @@ package br.com.catalisa.Data.Masking.API.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public class RequisicaoDadosSensiveis {
+
     @NotBlank(message = "CPF é obrigatório")
     private String cpf;
 
     @NotBlank(message = "Número do cartão é obrigatório")
     private String cartao;
 
-    public String getCpf() {
-        return cpf;
-    }
+    public RequisicaoDadosSensiveis() {}
 
-    public void setCpf(String cpf) {
+    public RequisicaoDadosSensiveis(String cpf, String cartao) {
         this.cpf = cpf;
-    }
-
-    public String getCartao() {
-        return cartao;
-    }
-
-    public void setCartao(String cartao) {
         this.cartao = cartao;
     }
+
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+
+    public String getCartao() { return cartao; }
+    public void setCartao(String cartao) { this.cartao = cartao; }
 }
